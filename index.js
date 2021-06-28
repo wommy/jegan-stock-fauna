27e6089ac2692759200e95eceade103219386151
wommy <tom@technomad.media>
2021-06-28 15:31:44 -0400

    feat: added table.quantity ala migration 2.2

--- index.js
+++ index.js
@@ -1,79 +1,62 @@
 const { Database, sql } = require('@leafac/sqlite')
 const database = new Database('stock.db')
 const howManyMigrationsRan = database.pragma("user_version", { simple: true })
+
 const migrations = [
 	()=>{
 		console.log("migration 1")
-		database.execute(
-			sql`CREATE TABLE IF NOT EXISTS "todos" (
+		database.execute( sql`
+			CREATE TABLE IF NOT EXISTS "todos" (
 				"id" INTEGER PRIMARY KEY AUTOINCREMENT,
 				"todo" TEXT NOT NULL,
-				"isDone" INTEGER DEFAULT 0);`,
-		),
-		database.run(
-			sql`INSERT INTO "todos" ("todo") VALUES (${"connect to sqlite3"})`
-		)
+				"isDone" INTEGER DEFAULT 0);
+		` ),
+		database.run( sql`INSERT INTO "todos" ("todo") VALUES (${"connect to sqlite3"});` )
+		console.log( database.all( sql`SELECT * FROM "todos"` ) )
 	},
+
 	()=>{
-		console.log("migration 2")
-		database.execute(
-			sql`CREATE TABLE IF NOT EXISTS "product-material" (
+		const seeds = [
+			{ product: "lighter", materials: ["blank", "art", "laminate", "bag"] },
+			{ product: "thank-you", materials: ["card","envelope"] },
+			{ product: "parcel", materials: ["mailer","business-card","miniprint", "sticker1", "sticker2"] }
+		]
+
+		console.log("migration 2.1")
+		database.execute( sql`
+			CREATE TABLE IF NOT EXISTS "product-material" (
 				"id" INTEGER PRIMARY KEY AUTOINCREMENT,
 				"name" TEXT NOT NULL,
 				"material" TEXT NOT NULL);`,
-		),
-		database.run( 
-			sql`INSERT INTO "product-material" ("name","material") VALUES (${"lighter"},${"blank"})`,
-		),
-		database.run( 
-			sql`INSERT INTO "product-material" ("name","material") VALUES (${"lighter"},${"art"})`,
-		),
-		database.run( 
-			sql`INSERT INTO "product-material" ("name","material") VALUES (${"lighter"},${"laminate"})`,
-		),
-		database.run( 
-			sql`INSERT INTO "product-material" ("name","material") VALUES (${"lighter"},${"bag"})`
-		),
-		database.run( 
-			sql`INSERT INTO "product-material" ("name","material") VALUES (${"thank-you"},${"card"})`,
-		),
-		database.run( 
-			sql`INSERT INTO "product-material" ("name","material") VALUES (${"thank-you"},${"envelope"})`
-		),
-		database.run( 
-			sql`INSERT INTO "product-material" ("name","material") VALUES (${"parcel"},${"mailer"})`
-		),
-		database.run( 
-			sql`INSERT INTO "product-material" ("name","material") VALUES (${"parcel"},${"business-card"})`,
-		),
-		database.run( 
-			sql`INSERT INTO "product-material" ("name","material") VALUES (${"parcel"},${"miniprint"})`
-		),
-		database.run( 
-			sql`INSERT INTO "product-material" ("name","material") VALUES (${"parcel"},${"sticker1"})`,
-		),
-		database.run( 
-			sql`INSERT INTO "product-material" ("name","material") VALUES (${"parcel"},${"sticker2"})`
 		)
-// keith put your code here
-	// },
-	// ()=>{
-	// 	console.log("migration 3")
-	// 	database.execute(
-	// 		sql``,
-	// 	),
-	// 	database.run( 
-	// 		sql``,
-	// 	)
+		seeds.map( seed => {
+			let product = seed.product
+			seed.materials.map( material => {
+				database.run( sql`INSERT INTO "product-material" ("name","material") VALUES (${product},${material});` )
+			})
+		})
+		console.log( database.all( sql`SELECT * FROM "product-material"` ) )
+
+		console.log("migration 2.2")
+		database.execute( sql`
+			CREATE TABLE IF NOT EXISTS "quantity" (
+				"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+				"name" TEXT NOT NULL,
+				"quantity" INTEGER DEFAULT 0);`,
+		)
+		seeds.map( seed => {
+			let quantity = [seed.product]
+			seed.materials.map( material => quantity.push(material) )
+			quantity.map( ea => {
+				database.run( sql`INSERT INTO "quantity" ("name") VALUES (${ea})` )
+			})
+		})
+		console.log( database.all( sql`SELECT * FROM "quantity"` ) )
 	}
 ]
+
 database.executeTransaction(()=>{
 	for (const migration of migrations.slice(howManyMigrationsRan))
 		migration()
 	database.pragma(`user_version = ${migrations.length}`)
 })
-
-const todos = database.all( sql`SELECT "todo" FROM "todos"` )
-console.log(todos)
-const products = database.all( sql`SELECT * FROM "product-material"` )
-console.log(products)
